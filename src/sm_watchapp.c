63cfd8808181045d47a128beb31a443f1d943868
Jason Ross <jason.ross@viasat.com>
2016-01-04 21:51:22 -0600

    Think i fixed the compilation issues
    moved declarations to sm_watchapp.c and extern declarations to the
    header file

--- src/sm_watchapp.c
+++ src/sm_watchapp.c
@@ -3,6 +3,30 @@
 #include "sm_watchapp.h"
 #include "sm_callbacks.h"
 
+char sm_string_buffer[STRING_LENGTH];
+char sm_weather_cond_str[STRING_LENGTH], sm_weather_temp_str[5];
+int sm_weather_img, sm_batteryPercent, sm_batteryPblPercent;
+
+char sm_calendar_date_str[STRING_LENGTH], sm_calendar_text_str[STRING_LENGTH];
+char sm_music_artist_str1[STRING_LENGTH], sm_music_title_str1[STRING_LENGTH];
+
+AppTimer *timerUpdateCalendar = NULL;
+AppTimer *timerUpdateWeather = NULL;
+AppTimer *timerUpdateMusic = NULL;
+
+
+const int WEATHER_IMG_IDS[] = {	
+  RESOURCE_ID_IMAGE_SUN,
+  RESOURCE_ID_IMAGE_RAIN,
+  RESOURCE_ID_IMAGE_CLOUD,
+  RESOURCE_ID_IMAGE_SUN_CLOUD,
+  RESOURCE_ID_IMAGE_FOG,
+  RESOURCE_ID_IMAGE_WIND,
+  RESOURCE_ID_IMAGE_SNOW,
+  RESOURCE_ID_IMAGE_THUNDER,
+  RESOURCE_ID_IMAGE_DISCONNECT
+};
+
 
 static uint32_t s_sequence_number = 0xFFFFFFFE;
 
